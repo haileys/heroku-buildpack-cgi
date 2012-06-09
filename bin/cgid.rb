feced3a108c73582ca5473534e1bb7609ea7cc92
Charlie Somerville <charlie@charliesomerville.com>
2012-06-10 01:18:38 +1000

    begin rescue end

--- bin/cgid.rb
+++ bin/cgid.rb
@@ -3,7 +3,10 @@ require "socket"
 
 serv = TCPServer.new ENV["PORT"]
 loop do
-  sock = serv.accept
-  sock.gets
-  sock.puts "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello world"
+  begin
+    sock = serv.accept
+    sock.gets
+    sock.puts "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello world"
+  rescue
+  end
 end
